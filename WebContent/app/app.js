98511c9b4bad5e06bccf6952a8cfffcfa03762f6
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-07-26 11:48:00 +0200

    M+

--- WebContent/app/app.js
+++ WebContent/app/app.js
@@ -27,15 +27,16 @@ async function buildContent(hash)
     content.empty();
 	try
 	{
-		let grammar1 = await getUrlContent("/menu.stxt");
-		let grammar2 = await getUrlContent("/module.stxt");
-		let grammar3 = await getUrlContent("/unit.stxt");
+		let ts = "?ts=" + new Date().getTime();
+		let grammar1 = await getUrlContent("/menu.stxt" + ts);
+		let grammar2 = await getUrlContent("/module.stxt" + ts);
+		let grammar3 = await getUrlContent("/unit.stxt" + ts);
 		
 		// Obtenemos content
 		if (hash.endsWith("/")) hash = hash + "index";
 		console.log("HASH = " + hash);
 		let stxtUrl = getUrlFromHash(hash);
-		let contentFromUrl = await getUrlContent(stxtUrl + "?ts=" + new Date());
+		let contentFromUrl = await getUrlContent(stxtUrl + ts);
 		
 		// Final
 	    const namespaceRetriever = new NamespaceRetriever();
