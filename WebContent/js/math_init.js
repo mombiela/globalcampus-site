74117b453d3a7d6c7bc2039c5a08b00dbae85d84
Joan Costa <joan.costa.mombiela@gmail.com>
2024-08-06 20:07:56 +0200

    Limpieza de código

--- WebContent/js/math_init.js
+++ WebContent/js/math_init.js
@@ -1,23 +1,23 @@
-    MathJax = {
-      loader: {load: ['input/tex', 'input/asciimath', 'output/chtml']},
-      tex: {
-        inlineMath: [['$', '$'], ['\\(', '\\)']],
-        displayMath: [['$$', '$$'], ['\\[', '\\]']]
-      },
-      asciimath: {
-        delimiters: [['`', '`']]
-      }
-    };
-  	function mathReload()
-  	{
-  		try
-  		{
-  			console.log("Cargando math....");
-  			MathJax.typeset();
-  			console.log("Math OK");
-  		}
-  		catch (e)
-  		{
-  			console.log(e);
-  		}
-  	}
+MathJax = {
+  loader: {load: ['input/tex', 'input/asciimath', 'output/chtml']},
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']]
+  },
+  asciimath: {
+    delimiters: [['`', '`']]
+  }
+};
+function mathReload()
+{
+	try
+	{
+		console.log("Cargando math....");
+		MathJax.typeset();
+		console.log("Math OK");
+	}
+	catch (e)
+	{
+		console.log(e);
+	}
+}
