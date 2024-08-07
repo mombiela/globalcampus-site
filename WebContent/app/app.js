9167e73f14158b51b4988a5516fb5f6d4ce7bc3c
Joan Costa <joan.costa.mombiela@gmail.com>
2024-08-07 11:02:25 +0200

    M+

--- WebContent/app/app.js
+++ WebContent/app/app.js
@@ -1,7 +1,4 @@
-import { NamespaceRetriever, STXTParser, getUrlContent } from '../js/stxt-parser.min.js';
-import { transform } from './transform.js';
-import { makeNavigation } from './navigation.js';
-import { getUrlFromHash, getHash } from './utils.js';
+import {buildContentFromHashUrl} from './build.js';
 
 document.addEventListener("DOMContentLoaded", ContentLoaded);
 
@@ -19,65 +16,4 @@ async function loadPage()
 {
     await buildContentFromHashUrl();
 }
-
-async function buildContentFromHashUrl()
-{
-    const content = $("#content");
-    //content.empty();
-	try
-	{
-	    const hash = getHash();
-		let stxtUrl = getUrlFromHash(hash);
-		let contentFromUrl = await getUrlContent(stxtUrl);
-		await buildContent(contentFromUrl, stxtUrl);
-	}
-	catch(exception)
-	{
-		console.log("Error: " + exception);
-		content.append($("<pre>").text("Page definition not valid: " + exception));
-	}
-}
-export async function buildContentFromString(str)
-{
-    const content = $("#content");
-    content.empty();
-	try
-	{
-	    const hash = getHash();
-		let stxtUrl = getUrlFromHash(hash);
-		await buildContent(str, stxtUrl);
-	}
-	catch(exception)
-	{
-		console.log("Error: " + exception);
-		content.append($("<pre>").text("Page definition not valid: " + exception));
-	}
-}
-
-export async function buildContent(content, stxtUrl)
-{
-	let grammar = await getUrlContent("/namespace.stxt");
-	
-	// Final
-    const namespaceRetriever = new NamespaceRetriever();
-	await namespaceRetriever.addGrammarDefinition(grammar);
-	
-	const parser = new STXTParser(namespaceRetriever);
-	const node = (await parser.parse(content))[0];
-	
-	// Make navigation
-	const navigation = await makeNavigation(getHash(), parser, node);
-
-	// Transform page
-	transform(node, navigation);
-	plantuml_runonce();
-	
-	// Insertamos en fuente
-	$("#link_source_code").attr("href", stxtUrl);
-	
-	// Mathjax
-	window["mathReload"]();
-}
-
-
 
--- WebContent/app/editor.js
+++ WebContent/app/editor.js
@@ -1,7 +1,7 @@
 import { mainEditor } from './template.js';
 import { getUrlFromHash, getHash } from './utils.js';
 import { getUrlContent } from '../js/stxt-parser.min.js';
-import { buildContentFromString } from './app.js';
+import { buildContentFromString } from './build.js';
 
 let lastVal = "";
 
