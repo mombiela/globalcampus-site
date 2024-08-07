f1701845f411d10181b35d68c43c615806496b9d
Joan Costa <joan.costa.mombiela@gmail.com>
2024-08-07 08:39:46 +0200

    M+

--- WebContent/app/editor.js
+++ WebContent/app/editor.js
@@ -17,12 +17,12 @@ async function initEditor()
 
 	$("#editor_textarea").val(contentFromUrl);
 
-    $('#editor_textarea').keydown(function(e) {
+    $("#editor_textarea").keydown(function(e) {
         if (e.key === 'Tab') {
             e.preventDefault();
-                    
-	        var start = this.selectionStart;
-    	    var end = this.selectionEnd;
+            
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
 
             // Set the value of the textarea to: text before caret + tab + text after caret
             $(this).val($(this).val().substring(0, start) + "\t" + $(this).val().substring(end));
@@ -40,21 +40,21 @@ async function initEditor()
 
             // Get the start of the current line
             var lineStart = value.lastIndexOf('\n', start - 1) + 1;
-                    
+            
             // Get the text of the current line
             var currentLine = value.substring(lineStart, start);
-                    
+            
             // Match leading tabs/spaces
-            var leadingTabs = currentLine.match(/^\t*/)[0];
+            var leadingWhitespace = currentLine.match(/^[\t ]*/)[0];
 
-            // Set the value of the textarea to: text before caret + newline + leading tabs + text after caret
-            var newValue = value.substring(0, start) + "\n" + leadingTabs + value.substring(end);
+            // Set the value of the textarea to: text before caret + newline + leading whitespace + text after caret
+            var newValue = value.substring(0, start) + "\n" + leadingWhitespace + value.substring(end);
 
             $(this).val(newValue);
 
-            // Put the caret at the right position again (after the newline and leading tabs)
-            this.selectionStart = this.selectionEnd = start + 1 + leadingTabs.length;
+            // Put the caret at the right position again (after the newline and leading whitespace)
+            this.selectionStart = this.selectionEnd = start + 1 + leadingWhitespace.length;
         }
-    });	
+    });
 }
 
